98555ca2be2351e76810585cc6e5eb86995728c2
Jimin <jjimini98@naver.com>
2024-06-05 20:56:31 +0900

    first commit

---
+++ convert_image.py
@@ -0,0 +1,34 @@
+import requests
+import uuid
+import time
+import json
+
+api_url = ''
+secret_key = ''
+image_file = 'test_image/test1.JPG'
+
+request_json = {
+    'images': [
+        {
+            'format': 'jpg',
+            'name': 'demo'
+        }
+    ],
+    'requestId': str(uuid.uuid4()),
+    'version': 'V1',
+    'timestamp': int(round(time.time() * 1000))
+}
+
+payload = {'message': json.dumps(request_json).encode('UTF-8')}
+files = [
+  ('file', open(image_file,'rb'))
+]
+headers = {
+  'X-OCR-SECRET': secret_key
+}
+
+response = requests.request("POST", api_url, headers=headers, data = payload, files = files)
+
+text = response.text.encode('utf8')
+
+print(text)
