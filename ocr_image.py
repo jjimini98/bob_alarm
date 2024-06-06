54443efe5b521403eed15703755d20d2d475e9d6
Jimin <jjimini98@naver.com>
2024-06-06 13:16:48 +0900

    feat : convert function to class

--- ocr_image.py
+++ ocr_image.py
@@ -5,6 +5,7 @@ import time
 import json
 from dotenv import load_dotenv
 from tqdm import tqdm 
+from collections import defaultdict
 load_dotenv(verbose=True) 
 
 
@@ -12,41 +13,81 @@ API  = os.getenv("INVOKE_URL")
 SECRETKEY = os.getenv("SECERT_KEY")
 
 
-def request_ocr(img : str): 
-    request_json = {
-        'images': [
-            {
-                'format': 'jpg',
-                'name': 'test'
-            }
-        ],
-        'requestId': str(uuid.uuid4()),
-        'version': 'V1',
-        'timestamp': int(round(time.time() * 1000))
-    }
+class OCR:
+    def request_img(self, img : str) -> str: 
+        """
+        clova ocr API 요청 보내기 
+        """
+        request_json = {
+            'images': [
+                {
+                    'format': 'jpg',
+                    'name': 'test'
+                }
+            ],
+            'requestId': str(uuid.uuid4()),
+            'version': 'V1',
+            'timestamp': int(round(time.time() * 1000))
+        }
 
-    payload = {'message': json.dumps(request_json).encode('UTF-8')}
-    files = [
-    ('file', open(img,'rb'))
-    ]
-    headers = {
-    'X-OCR-SECRET': SECRETKEY
-    }
+        payload = {'message': json.dumps(request_json).encode('UTF-8')}
+        files = [
+        ('file', open(img,'rb'))
+        ]
+        headers = {
+        'X-OCR-SECRET': SECRETKEY
+        }
 
-    response = requests.request("POST", API, headers=headers, data = payload, files = files)
+        response = requests.request("POST", API, headers=headers, data = payload, files = files)
 
-    return response.text 
+        return response.text        
 
-def extract_json():
+
+    def extract_response(self,result : str):
+        result = json.loads(result)  #str을 dict로 변경 
+        extract = dict()
+        ocr_result = result.get("images")[0]
+        daily_bob = ocr_result.get("fields")
+        for daily in daily_bob:
+            text = daily.get("inferText")
+            extract[daily.get("name")] = text 
+
+        return extract
+    
+
+    def split_data(self,extract_data : dict):
+        weekly_menu = defaultdict(dict)
+
+        for k,v in extract_data.items():
+            split_dict = dict()
+            if "그린샐러드&드레싱\n" in v : 
+                split_dict["점심"] = v.split("그린샐러드&드레싱\n")[0]
+                split_dict["저녁"] = v.split("그린샐러드&드레싱\n")[1]
+            elif "바나나\n" in v:
+                split_dict["점심"] = v.split("바나나\n")[0]
+                split_dict["저녁"] = v.split("바나나\n")[1]
+            weekly_menu[k] = split_dict
+        
+
+        return weekly_menu    
+
+    def convert_json(self, menu : dict , img_name : str) : 
+        with open(f'./test_result/{img_name}.json','w') as f:
+                json.dump(menu, f, ensure_ascii=False, indent=4)
+        
+        print(f"==================={img_name} 변환 완료===================")
+
+
+
+if __name__ == "__main__":
+    # 여기서 이미지를 직접 넣어준다 
+    ocr = OCR()
     for img in tqdm(os.listdir("test_image")):
         if img != ".DS_Store":
-            img_name = img.split(".")[0]
-            img_path = f"test_image/{img}"
-            result = request_ocr(img_path) 
-            time.sleep(1) #1분 30초 정도 소요됨
-            with open(f'./test_result/{img_name}.json','w') as f:
-                json.dump(result, f, ensure_ascii=False, indent=4)
+            imgname = img.split(".")[0]
+            res = ocr.request_img(f"test_image/{imgname}.JPG")
+            ees = ocr.extract_response(res)
+            menu = ocr.split_data(ees)
+            ocr.convert_json(menu, imgname)
 
 
-if __name__ == "__main__": 
-    extract_json()
