453b7db6345b767862d1121dfef82acfb982d7bc
Jimin <jjimini98@naver.com>
2024-08-11 14:08:23 +0900

    refactor : 코드 리팩토링 함수 간 종속성 제거

--- request_image.py
+++ request_image.py
@@ -4,11 +4,8 @@ import uuid
 import time 
 import json
 from dotenv import load_dotenv
-from tqdm import tqdm 
-from collections import defaultdict
-from datetime import datetime
 import pymongo
-load_dotenv(verbose=True) 
+load_dotenv(verbose=True)
 
 """
 clova OCR API로 이미지를 읽어와서 json 파일로 저장합니다.
@@ -18,12 +15,12 @@ clova OCR API로 이미지를 읽어와서 json 파일로 저장합니다.
 class Request:
     def __init__(self,img):
         self.image = img
-        self.API  = os.getenv("ADD_INVOKE_URL")
-        self.SECRETKEY = os.getenv("ADD_SECERT_KEY")
+        self.API  = os.getenv("INVOKE_URL")
+        self.SECRETKEY = os.getenv("SECERT_KEY")
 
-    def request_image(self) -> str: 
+    def request_image(self) -> str:
         """
-        clova ocr API 요청 보내기 
+        clova ocr API 요청 보내고, str 의 형태로 결과를 받는다.
         """
         request_json = {
             'images': [
@@ -46,51 +43,58 @@ class Request:
         }
 
         response = requests.request("POST", self.API, headers=headers, data = payload, files = files)
-        # print(response.text)
-        return response.text        
+        return response.text
 
-    def transform_result(self):
+
+
+    def refine_data(self,text : str) -> dict:
         """
-        인식된 결과를 {요일 : 메뉴} 의 형태로 리턴해준다. 
+        response.text로 받은 문자열을 json 형식으로 변경
+        {요일 : inferText} 의 형태로 반환
+
         """
-        # str -> dict 
-        result = json.loads(self.request_image()) 
         weekly_menu = dict()
+        response = json.loads(text)
 
-        extract_result = result['images'][0]["fields"]
-        for result in extract_result:
-            weekday = result['name']
-            menu = result['inferText']
+        for field in response['images'][0]["fields"]:
+            weekday = field['name']
+            menu = field.get("inferText")
+
             weekly_menu[weekday] = menu
 
         return weekly_menu
-    
 
-    def convert_json(self,filename):
-        with open(f"test_result/{filename}.json", "w") as f: 
-            json.dump(self.transform_result(), f, ensure_ascii=False, indent=4)
-        return "=================CONVERT JSON COMPLETE================="
-
+    def convert_json(self,refine_dict : dict) :
+        """
+        딕셔너리를 넣으면 정해진 파일 이름으로 json 파일을 만들어 준다.
+        """
+        with open(f"test_result/{filename}.json", 'w') as f:
+            json.dump(refine_dict,f,ensure_ascii=False, indent=4)
+        print("=================CONVERT JSON COMPLETE=================")
 
     def load_db(self,filenumber):
         conn = pymongo.MongoClient(host= "localhost", port=27017)
         db = conn["menus"]
         col = db["menu"]
-        with open(f"test_result/test{filenumber}.json", "r") as f: 
+        with open(f"test_result/test{filenumber}.json", "r") as f:
             json_data = json.load(f)
             for _,v in json_data.items():
-                new_json = {} 
+                new_json = {}
                 v = v.split("\n")
                 new_json[v[0]] = ' '.join(v[1:])
                 x = col.insert_one(new_json)
                 print(x)
-            
-    
+
 
 
 if __name__ == "__main__":
-    # 테스트 이미지 수 만큼 반복문 돌리기 
-        ocr = Request(f"test_image/test21.png")
-        ocr.convert_json(f"test21")
-        ocr.load_db("21")
+        filename = "test26"
+        ocr = Request(f"test_image/{filename}.png")
+        result = ocr.request_image()
+        refine_dict = ocr.refine_data(result)
+        ocr.convert_json(refine_dict)
+
+
+            
+
     
